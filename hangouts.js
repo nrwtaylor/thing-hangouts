27549bee90c0a36a492845d1eb65a00b8949e51b
Nick Taylor <nrwtaylor@gmail.com>
2019-08-27 19:33:39 -0700

    Agents

--- hangouts.js
+++ hangouts.js
@@ -5,6 +5,10 @@ var gearman_client = gearmanode.client();
 var Client = require('hangupsjs');
 var Q = require('q');
 
+//var linkify = require('linkifyjs');
+//require('linkifyjs/plugins/hashtag')(linkify); // optional
+//var linkifyHtml = require('linkifyjs/html');
+
 const var_dump = require('var_dump')
 
 // Turn off Gearman node debug
@@ -32,7 +36,7 @@ client.connect(creds).then(function() {
 
 var list = new Array(45)
 client.on("chat_message", function(ev) {
-  console.log('chat_message');
+  console.log('Received a chat message.');
 
   if (ev.chat_message.message_content.segment &&
       ev.self_event_state.user_id.gaia_id != ev.sender_id.gaia_id) {
@@ -45,41 +49,150 @@ client.on("chat_message", function(ev) {
     list.push(event_id)
     list.shift()
 
-    var to = ev.sender_id.gaia_id;
-    var from = ev.conversation_id.id;
+    var_dump(ev);
+
+//    var to = ev.sender_id.gaia_id;
+//    var from = ev.conversation_id.id;
+
+    var to = ev.conversation_id.id;
+    var from = "agent";
+
+//    var from = "elden";
+//    var to = ev.conversation_id.id;
 
     var subject = ev.chat_message.message_content.segment[0].text;
+
+    var word_count = subject.split(" ").length;
+
     var match = false
 
+    var agent_name = "agent";
+
+    console.log("Counted " + word_count + " words");
     if (subject.toLowerCase().includes('kaiju')) {
-      var ret = subject.replace(/kaiju/g,'');
+      if (word_count > 1) {
+        var pattern = new RegExp("kaiju", 'gi');
+        var subject = subject.replace(pattern, '');
+//        var subject = subject.replace(/kaiju/g,'');
+      }
       match = true;
     }
-
+    console.log("subject " +subject);
     if (subject.toLowerCase().includes('elden')) {
-      var ret = subject.replace(/elden/g,'');
+      var agent_name = "elden";
+      if (word_count > 1) {
+        var subject = subject.replace(/elden/g,'');
+      }
       match = true;
     }
 
-    if (subject.toLowerCase().includes('hythe')) {
-      var ret = subject.replace(/hythe/g,'');
+    if (subject.toLowerCase().includes('hal9000')) {    
+      var agent_name = "hal9000";
+      if (word_count > 1) {
+        var subject = subject.replace(/hal9000/g,'');
+      }
+      match = true;
+    }
+
+    if (subject.toLowerCase().includes('hal')) {    
+      var agent_name = "hal9000";
+      if (word_count > 1) {
+        var subject = subject.replace(/hal/g,'');
+      }
+      match = true;
+    }
+
+
+    if (subject.toLowerCase().includes('edna bot')) {
+      var agent_name = "edna";
+      if (word_count > 1) {
+        var subject = subject.replace(/edna bot/g,'');
+      }
+      match = true;
+    }
+
+
+    if (subject.toLowerCase().includes('ednabot')) {
+      var agent_name = "edna";
+      if (word_count > 1) {
+        var subject = subject.replace(/ednabot/g,'');
+      }
       match = true;
     }
 
     if (subject.toLowerCase().includes('edna')) {
-      var ret = subject.replace(/edna/g,'');
+      var agent_name = "edna";
+      if (word_count > 1) {
+        var subject = subject.replace(/edna/g,'');
+      }
       match = true;
     }
 
-    if (match == false) {return;}
+    if (subject.toLowerCase().includes('control')) {
+      if (word_count ==1) {
+        match = true;
+        var agent_name = "control";
+      }
+    }
+
+    if (subject.toLowerCase().includes('contact')) {
+      if (word_count ==1) {
+        match = true;
+        var agent_name = "control";
+      }
+    }
+
+    if (subject.toLowerCase().includes('break')) {
+      if (word_count ==1) {
+        match = true;
+        var agent_name = "control";
+      }
+    }
+
+    if (subject.toLowerCase().includes('hythe bot')) {
+      var agent_name = "hythebot";
+      if (word_count > 1) {
+        var subject = subject.replace(/hythe bot/g,'');
+      }
+      match = true;
+    }
+
+    if (subject.toLowerCase().includes('hythebot')) {    
+      var agent_name = "hythebot";
+      if (word_count > 1) {
+        var subject = subject.replace(/hythebot/g,'');
+      }
+      match = true;
+    }
+
+    if (subject.toLowerCase().includes('hythe')) {
+      var agent_name = "hythebot";
+      if (word_count > 1) {
+        var subject = subject.replace(/hythe/g,'');
+      }
+      match = true;
+    }
+
+
+//    var_dump(match);
+
+    if (match == false) {
+      console.log("No agent recognized. Datagram ignored.");
+      return;
+    }
+
+    console.log("Reading datagram.");
 
     var arr = {"from":from,"to":to,"subject":subject}
     var datagram = JSON.stringify(arr)
 
     try {
       var job = gearman_client.submitJob('call_agent', datagram);
+      console.log("Send to Gearman.");
+
     }
     catch (e) {
+      console.log("Error from Gearman.");
       var sms = "quiet"
       var message = "Quietness. Just quietness."
     }
@@ -89,7 +202,7 @@ client.on("chat_message", function(ev) {
     });
 
     job.on('complete', function() {
-
+console.log("Gearman response completed.");
       sms = "sms"
       message = "sms"
 
@@ -100,14 +213,29 @@ client.on("chat_message", function(ev) {
       }
 
       catch (e) {
+        console.log("Error parsing Gearman response.");
         var sms = "quiet"
         var message = "Quietness. Just quietness."
       }
 
 //      var sms = thing_report.sms
 //      var message = thing_report.message
+console.log("to " + to);
+console.log("sms " + sms);
+console.log("Done");
 
-      return client.sendchatmessage(from, [[0, sms]]);
+//sms = linkifyHtml(sms, {
+//  defaultProtocol: 'http'
+//});
+
+if (sms == null) {sms = agent_name.toUpperCase() + " | ?";}
+var response = [[0, sms]]; 
+var_dump(response);
+      return client.sendchatmessage(to, response);
+
+//      return client.sendchatmessage(to, [[0, sms]]);
+
+//      return client.sendchatmessage(from, [[0, sms]]);
     });
   }
 });
